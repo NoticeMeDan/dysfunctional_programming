4c1fe6fdc203a81cc77f2414f4cecf11640883c2
Vilfred <Vilfredsikker@gmail.com>
2019-02-12 15:00:14 +0100

    added exercise 2.2 and 2.3

--- Assignment02/Library.fs
+++ Assignment02/Library.fs
@@ -1,14 +1,28 @@
 ﻿namespace Assignment02
 
 module wee =
-    // 2.1
+    // Exercise 2.1
     let rec downto1 n = 
         if n > 0
         then n::downto1(n-1)
         else []
     
-    // 2.2
     let rec downto2 n =
         match n with
         | 0 -> []
         | n -> n::downto1(n-1)
+        
+    // Exercise 2.2
+    let rec removeOddIdx =
+        function
+        | [] -> []
+        | [x] -> [x]
+        | x :: y :: xs -> x :: removeOddIdx xs
+        
+        
+    // Exercise 2.3
+    let rec combinePair t =
+        match t with
+        | [] -> []
+        | [x1] -> []
+        | x1::x2::tt -> (x1, x2) :: combinePair tt
