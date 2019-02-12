50a64c7873cae6c725bbd46e7700dbc607610871
Elias Jørgensen <eliaszakarias@gmail.com>
2019-02-12 17:50:24 +0100

    Implement 2.5

--- Assignment02/Library.fs
+++ Assignment02/Library.fs
@@ -26,3 +26,19 @@ module wee =
         | [] -> []
         | [x1] -> []
         | x1::x2::tt -> (x1, x2) :: combinePair tt
+    
+    // Exercise 2.4
+    
+    // Exercise 2.5
+    type complex = float * float
+    
+    let mkComplex x y = complex (x, y)
+    
+    let (|+|) ((a, b): complex) ((c, d): complex) = complex (a + c, b + d)
+    let (|-|) ((a, b): complex) ((c, d): complex) = (a, b) |+| (-c, -d)
+    
+    let (|*|) ((a, b): complex) ((c, d): complex) = complex ((a * c) - (b * d), (b * c) + (a * d))
+        
+    let (|/|) ((a, b): complex) ((c, d): complex) =
+        let divisor = c ** 2. + d ** 2.
+        complex (a, b) |*| (c / divisor, -d / divisor)
