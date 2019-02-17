eb8984bb3e9527c5af831e69b943429ce40a6949
Elias Jørgensen <eliaszakarias@gmail.com>
2019-02-17 18:45:04 +0100

    Implement 3.2 and 3.3

--- Assignment03/Library.fs
+++ Assignment03/Library.fs
@@ -1,20 +1,37 @@
 ﻿namespace Assignment03
 
 module Say =
-    // 3.1
-    type 'a bintree = 
+    type 'a bintree when 'a : comparison =
     | Leaf
-    | Node of 'a * 'a bintree * 'a bintree
-
+    | Node of ('a * 'a bintree * 'a bintree)
+    
     let rec insert x =
-        function
-        | Leaf                       -> Node(x, Leaf, Leaf)
-        | Node (y, l, r) when x <= y -> Node(y, insert x l, r)
-        | Node (y, l, r)             -> Node(y, l, insert x r)
+        function 
+        | Leaf                       -> Node (x, Leaf, Leaf)
+        | Node (y, l, r) when x <= y -> Node (y, insert x l, r)
+        | Node (y, l, r)             -> Node (y, l, insert x r)
     
     let rec inOrder =
         function
         | Leaf           -> []
-        | Node (x, l, r) -> inOrder l @ x :: inOrder r
-        
-    let binSort list = List.foldBack insert list Leaf |> inOrder
+        | Node (x, l, r) -> inOrder l @ x::inOrder r
+     
+    // 3.1
+    let binarySort list = List.foldBack insert list Leaf |> inOrder
+    
+    // 3.2
+    let rec mapInOrder f =
+            function
+            | Leaf -> Leaf
+            | Node(x, l, r) -> 
+                let temp = mapInOrder f l
+                Node(f x, temp, mapInOrder f r)
+    
+    // 3.3
+    let rec foldInOrder func init =
+        function
+        | Leaf -> init
+        | Node(x, l, r) ->
+            foldInOrder func init l
+            |> (fun init -> func x init)
+            |> (fun init -> foldInOrder func init r)
