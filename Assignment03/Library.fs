ac166c726af581f4be5cd0d4ab51663c7ed60b75
Elias Jørgensen <eliaszakarias@gmail.com>
2019-02-17 17:16:35 +0100

    Add project for assignment03, and implement 3.1

---
+++ Assignment03/Library.fs
@@ -0,0 +1,20 @@
+﻿namespace Assignment03
+
+module Say =
+    // 3.1
+    type 'a bintree = 
+    | Leaf
+    | Node of 'a * 'a bintree * 'a bintree
+
+    let rec insert x =
+        function
+        | Leaf                       -> Node(x, Leaf, Leaf)
+        | Node (y, l, r) when x <= y -> Node(y, insert x l, r)
+        | Node (y, l, r)             -> Node(y, l, insert x r)
+    
+    let rec inOrder =
+        function
+        | Leaf           -> []
+        | Node (x, l, r) -> inOrder l @ x :: inOrder r
+        
+    let binSort list = List.foldBack insert list Leaf |> inOrder
