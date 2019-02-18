15b10f24ea3faa014504a7a781c5deed825d4da9
Elias Jørgensen <eliaszakarias@gmail.com>
2019-02-18 18:32:04 +0100

    Add skeleton for 3.4

--- Assignment03/Library.fs
+++ Assignment03/Library.fs
@@ -35,3 +35,58 @@ module Say =
             foldInOrder func init l
             |> (fun init -> func x init)
             |> (fun init -> foldInOrder func init r)
+            
+    // 3.4
+    type aExp =          (* arithmetical expressions *)
+    | N of int           (* numbers *)
+    | V of string        (* variables *)
+    | Add of aExp * aExp (* addition *)
+    | Mul of aExp * aExp (* multiplication *)
+    | Sub of aExp * aExp (* subtraction *)
+
+    let unop  f a s   = f (a s)
+    let binop f a b s = f (a s) (b s)
+
+    let rec A =
+        function
+        | N n          -> fun _ -> n
+        | V x          -> Map.find x
+        | Add (a1, a2) -> binop ( + ) (A a1) (A a2)
+        | Mul (a1, a2) -> binop ( * ) (A a1) (A a2)
+        | Sub (a1, a2) -> binop ( - ) (A a1) (A a2)
+
+    type bExp =          (* boolean expressions *)
+    | TT                 (* true *)
+    | FF                 (* false *)
+    | Eq of aExp * aExp  (* numeric equality *)
+    | Lt of aExp * aExp  (* numeric less than *)
+    | Neg of bExp        (* boolean not *)
+    | Con of bExp * bExp (* boolean conjunction *)
+
+    (* TODO: Write a function B : bExp -> state -> bool
+             to evaluate boolean expressions (note that you will need to refer to A) *)
+
+
+    let rec B _ = failwith "Not implementd"
+
+
+    type stm =                (* statements *)
+    | Ass of string * aExp    (* variable assignment *)
+    | Skip                    (* nop *)
+    | Seq of stm * stm        (* sequential composition *)
+    | ITE of bExp * stm * stm (* if-then-else statement *)
+    | While of bExp * stm     (* while statement *)
+
+    let update = Map.add
+
+    (* TODO: Write I : stm -> state -> state. 
+             You can rewrite the skeleton if you want, but the signature must be the same *)
+
+
+    let rec I =
+      function 
+      | Ass (x,a)         -> failwith "Not implemented" (* use update *)
+      | Skip              -> failwith "Not implemented"
+      | Seq (stm1, stm2)  -> failwith "Not implemented"
+      | ITE (b,stm1,stm2) -> failwith "Not implemented"
+      | While (b, stm)    -> failwith "Not implemented"
