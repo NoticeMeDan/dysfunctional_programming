5f3c12b47092001d39f3262360d83b7f1f4a88e0
Vilfred <Vilfredsikker@gmail.com>
2019-02-20 14:10:13 +0100

    3.6 working

--- Assignment03/Library.fs
+++ Assignment03/Library.fs
@@ -43,15 +43,28 @@ module Say =
         | Add of aExp * aExp (* addition *)
         | Mul of aExp * aExp (* multiplication *)
         | Sub of aExp * aExp (* subtraction *)
-
+        | Inc of string
+    
+    let binup eval a1 a2 state op =
+        let eval1, state1 = eval a1 state
+        let eval2, state2 = eval a2 state1
+        (op eval1 eval2, state2)
+                                    
+    let unop eval a state op =
+        let eval1, state1 = eval a state
+        (op eval1, state1)
+    
     let rec A e s =
         match e with
-        | N n          -> n
-        | V x          -> Map.find x s
-        | Add (a1, a2) -> A a1 s + A a2 s
-        | Mul (a1, a2) -> A a1 s * A a2 s
-        | Sub (a1, a2) -> A a1 s - A a2 s
-        
+        | N n          -> (n, s)
+        | V x          -> (Map.find x s, s)
+        | Add (a1, a2) -> binup A a1 a2 s (+) 
+        | Mul (a1, a2) -> binup A a1 a2 s (*) 
+        | Sub (a1, a2) -> binup A a1 a2 s (-)
+        | Inc x        -> let xval = Map.find x s
+                          let newState = Map.add x (xval+1) s
+                          (xval+1, newState)
+                          
     type bExp =          (* boolean expressions *)
         | TT                 (* true *)
         | FF                 (* false *)
@@ -62,12 +75,12 @@ module Say =
 
     let rec B e s =
         match e with
-        | TT          -> true
-        | FF          -> false
-        | Eq (a1, a2) -> A a1 s = A a2 s
-        | Lt (a1, a2) -> A a1 s < A a2 s
-        | Neg (b1)    -> not (B b1 s)
-        | Con (b1, b2) -> (B b1 s) && (B b2 s)
+        | TT          -> (true, s)
+        | FF          -> (false, s)
+        | Eq (a1, a2) -> binup A a1 a2 s (=) 
+        | Lt (a1, a2) -> binup A a1 a2 s (<)
+        | Neg (b1)    -> unop B b1 s not
+        | Con (b1, b2) -> binup B b1 b2 s (&&)
 
     type stm =                (* statements *)
         | Ass of string * aExp    (* variable assignment *)
@@ -81,11 +94,22 @@ module Say =
     let update = Map.add
 
     let rec I stm s =
-      match stm with
-      | Ass (x, a)         -> update (x) (A a s) s
-      | Skip               -> s
-      | Seq (stm1, stm2)   -> (I stm1 >> I stm2) s
-      | ITE (bExp, stm1, stm2)-> if (B bExp s) then I stm1 s else I stm2 s
-      | While (bExp, stm1) -> I (ITE (bExp, Seq(stm1, stm), Skip)) s
-      | IT (bExp, stm)        -> if (B bExp s) then I stm s else I Skip s
-      | Repeat (stm, bExp)    -> I(Seq (stm, While(bExp, stm))) s
+        match stm with
+        | Ass (x, a)         -> (A a s) ||> update (x)
+        | Skip               -> s
+        | Seq (stm1, stm2)   -> (I stm1 >> I stm2) s
+        | ITE (bExp, stm1, stm2)->
+            let (bool,state) = (B bExp s)
+            if bool then I stm1 state else I stm2 state
+        | While (bExp, stm1) -> I (IT (bExp, Seq(stm1, stm))) s
+        | IT (bExp, stm)        -> I (ITE(bExp, stm, Skip)) s
+        | Repeat (stm, bExp)    -> I(Seq (stm, While(bExp, stm))) s
+      
+    let factorial x =
+        ITE (Neg (Lt (N x, N 0)),
+             Seq (Seq (Ass ("result", N 1), Ass ("x", N 0)),
+                  While (Neg (Eq (Add (N x, N 1), Inc "x")),
+                         Ass ("result", Mul (V "result", V "x")))), 
+             Skip)
+
+    printfn "%A" (I (factorial 0) Map.empty |> Map.toList |> List.sortBy fst) 
