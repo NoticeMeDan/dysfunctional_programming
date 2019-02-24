40ba53053c8cba402962508e0aeea4435c9b9bf2
Elias Jørgensen <eliaszakarias@gmail.com>
2019-02-24 17:54:32 +0100

    Fix empty function

--- Assignment04/MultiSet.fs
+++ Assignment04/MultiSet.fs
@@ -2,6 +2,6 @@ module Assignment04.MultiSet
 
     type MultiSet<'a> when 'a : comparison = Map<'a, uint32>
     
-    let empty = MultiSet<'a>
+    let empty :MultiSet<'a> = Map.empty<'a, uint32>
     
     let isEmpty (set : MultiSet<'a>) = Map.isEmpty set
