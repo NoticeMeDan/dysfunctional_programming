0192c1cfc480973d3d4fe845e9bcde1c5754fb34
Elias Jørgensen <eliaszakarias@gmail.com>
2019-02-24 18:29:37 +0100

    add mo stuff

--- Assignment04/MultiSet.fs
+++ Assignment04/MultiSet.fs
@@ -5,3 +5,30 @@ module Assignment04.MultiSet
     let empty :MultiSet<'a> = Map.empty<'a, uint32>
     
     let isEmpty (set : MultiSet<'a>) = Map.isEmpty set
+    
+    let size (set: MultiSet<'a>) = Map.fold (fun state key value -> state + value) 0u set
+    
+    let contains key (set: MultiSet<'a>) = Map.containsKey key set
+    
+    let numItems key (set: MultiSet<'a>) = Map.find key set
+    
+    let add key num (set: MultiSet<'a>) =
+        if contains key set then
+            let prevNum = numItems key set
+            Map.add key (num + prevNum) set
+        else 
+            Map.add key num set
+    
+    let addSingle key set = add key 1u set
+    
+    let remove key num (set: MultiSet<'a>) =
+        if contains key set then
+            let prevNum = numItems key set
+            if num > prevNum then
+                Map.remove key set
+            else
+                Map.add key (prevNum - num) set
+        else
+            set
+    
+    let removeSingle key set = remove key 1u set
