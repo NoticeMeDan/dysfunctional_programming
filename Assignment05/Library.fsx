3ade255c545307c9c1fa828bf9cf89a9c4001e13
Vilfred <Vilfredsikker@gmail.com>
2019-02-28 21:08:10 +0100

    changed file to a script file

---
+++ Assignment05/Library.fsx
@@ -0,0 +1,15 @@
+﻿let sum m n =
+    let rec loop x acc =
+        match x with 
+        | 0 -> acc + m
+        | _ -> loop (x - 1) (acc + m + x)
+    loop n 0
+    
+let length lst =
+    let rec loop x acc  =
+        match x with
+        | [] -> acc
+        | x::xs -> loop xs (acc + 1)
+    loop lst 0
+    
+    
