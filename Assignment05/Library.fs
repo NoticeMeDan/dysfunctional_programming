7e8ec72733c0adf15ba78e047665df3330e6eadc
Vilfred <Vilfredsikker@gmail.com>
2019-02-28 20:39:35 +0100

    passes first exercise

---
+++ Assignment05/Library.fs
@@ -0,0 +1,11 @@
+﻿namespace Assignment05
+
+module Say =
+    
+    let sum m n =
+        let rec loop x acc =
+            match x with 
+            | 0 -> acc
+            | _ -> loop (x - 1) (acc + m + (x))
+        loop n 0
+        
