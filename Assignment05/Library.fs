df5fe477a01e3906724f8f8146f96793b9c844cc
Vilfred <Vilfredsikker@gmail.com>
2019-02-28 21:00:28 +0100

    passes exercise 2

--- Assignment05/Library.fs
+++ Assignment05/Library.fs
@@ -9,3 +9,11 @@ module Say =
             | _ -> loop (x - 1) (acc + m + (x))
         loop n 0
         
+    let length lst =
+        let rec loop x acc  =
+            match x with
+            | [] -> acc
+            | x::xs -> loop xs (acc + 1)
+        loop lst 0
+        
+        
